910bdd7da0f3787bc0e3e96ca6502eebf46093b1
psyms21 <psyms21@nottingham.ac.uk>
2025-09-05 14:39:11 +0100

    remove redundant print

--- stockbot.py
+++ stockbot.py
@@ -8,19 +8,21 @@ recipient_emails = emails_str.split(",") if emails_str else []
 
 if __name__ == '__main__':
     # check stock via webpages
-    filename, save_path = stock_check()
+    # filename, save_path = stock_check()
 
-    # request token
-    token = "Bearer "
-    token += get_tenant_access_token(app_id, app_secret)
+    print("test")
 
-    # upload latest stock statuses
-    file_key = upload(token, filename, save_path)
+    # # request token
+    # token = "Bearer "
+    # token += get_tenant_access_token(app_id, app_secret)
 
-    # compare current stock status with previous
-    summary = stock_compare()
+    # # upload latest stock statuses
+    # file_key = upload(token, filename, save_path)
 
-    # send stock spreadsheet and summary of changes to recipients via bot
-    for email in recipient_emails:
-        send_file(token, email, file_key)
-        send(token, email, summary)
+    # # compare current stock status with previous
+    # summary = stock_compare()
+
+    # # send stock spreadsheet and summary of changes to recipients via bot
+    # for email in recipient_emails:
+    #     send_file(token, email, file_key)
+    #     send(token, email, summary)

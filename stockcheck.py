83bd2ce450dfe5998d33b1a9e93af72acc7a558f
psyms21 <psyms21@nottingham.ac.uk>
2025-09-05 14:54:27 +0100

    Revert "reenable headless"
    This reverts commit 5c3ca082dd846b93240293ceac90e0452402e9d7.

--- stockcheck.py
+++ stockcheck.py
@@ -15,7 +15,7 @@ from urllib3.exceptions import ReadTimeoutError
 import time
 
 options = Options()
-options.add_argument("--headless=new")   # important for GitHub Actions
+# options.add_argument("--headless=new")   # important for GitHub Actions
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
